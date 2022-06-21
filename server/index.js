73de7392fffdce2691f53616d0d673306a7cac97
STEIN <doruchenko.m.i@gmail.com>
2022-06-21 16:21:52 +0300

    front ready

--- server/index.js
+++ server/index.js
@@ -1,35 +1,34 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
-const sequelize = require("./db");
-const models = require("./models/models");
-const cors = require("cors");
-const router = require("./routes/index");
-const errorHandler = require("./middleware/ErrorHandlingMiddleware");
+const PORT = process.env.PORT || 5000
 
-const PORT = process.env.PORT || 5000;
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/api", router);
-app.use(errorHandler);
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+app.use('/api', router)
 
-// app.get("/", (req, res) => {
-//   res.status(200).json({
-//     message: "Hello",
-//   });
-// });
+// Обработка ошибок, последний Middleware
+app.use(errorHandler)
 
 const start = async () => {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync();
-    app.listen(PORT, () => {
-      console.log(`Example app listening on port ${PORT}`);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
 
-start();
+
+start()
